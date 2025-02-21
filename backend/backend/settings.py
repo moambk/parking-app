1b329ff76ba427eea3b711e439571913be6a7b59
AmirBekhouche <mohamed.bekhouche@etu.univ-smb.fr>
2025-02-21 17:30:57 +0100

    finish

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h%(tp6v%apqx%&xwnjx#&ho3o!$u7ec8%%l2+%pu!*4bqj&9x)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['parking-app-v9k6.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
